ca341737b9ac6a61b83c4ab50f6294af29ea1608
Art1us <109896229+Art1us@users.noreply.github.com>
2022-09-03 15:01:06 +0200

    Update CheckoutProduct.js

--- src/CheckoutProduct.js
+++ src/CheckoutProduct.js
@@ -3,18 +3,20 @@ import "./CheckoutProduct.css";
 import { useStateValue } from "./StateProvider";
 
 function CheckoutProduct({ id, image, title, price, rating }) {
-const [{basket}, dispatch] = useStateValue()
+  const [{ basket }, dispatch] = useStateValue();
 
- const removeFromBasket = ()=>{
-        dispatch({
-            type: 'REMOVE_FROM_BASKET',
-            id:id,
-        })
- }
- 
-    return (
+  const removeFromBasket = () => {
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      id: id,
+    });
+  };
+
+  return (
     <div className="checkoutProduct">
-      <img className="checkoutProduct__image" src={image} alt={title} />
+      <div className="checkoutProduct__imgContainer">
+        <img className="checkoutProduct__image" src={image} alt={title} />
+      </div>
 
       <div className="checkoutProduct__info">
         <p className="checkoutProduct__title">{title}</p>
